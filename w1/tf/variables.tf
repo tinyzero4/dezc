49c1236398a42805bc51f40e9e7f2a518c128dab
dzmitry <dzmitry.zenin@gmail.com>
2024-01-26 02:08:35 +0100

    w1 solutions

---
+++ w1/tf/variables.tf
@@ -0,0 +1,40 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "~/Downloads/dezoomcamp-411812-703b42cb8f24.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "dezoomcamp-411812"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "dezc-2024-curlydz"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
